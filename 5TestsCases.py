9a240710f525c56af50e4048cfb8372f84ab9e70
Volkova Natalya <nata.volkowa@yandex.ru>
2016-04-22 22:57:28 +0600

    Update 5TestsCases.py
    Постаралась изменить согласно замечаниям

--- 5TestsCases.py
+++ 5TestsCases.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,6 +12,20 @@ class FiveTestsCases(TestCase):
         self.driver.implicitly_wait(5)
         self.driver.get('http://2gis.ru/novosibirsk')
         self.driver.find_element_by_class_name('searchBar__buttons').click()
+        self.search_bar_from_field = self.driver.find_element_by_css_selector("div[class='searchBar__textfield _from'] input[class='suggest__input']")
+        self.search_bar_to_field = self.driver.find_element_by_css_selector("div[class='searchBar__textfield _to'] input[class='suggest__input']")
+
+    def web_driver_wait(self, value, obj):
+        WebDriverWait(self.driver, value).until(EC.element_to_be_clickable((By.CSS_SELECTOR, obj)))
+
+    def zoom(self, value):
+        zoom_button = self.driver.find_element_by_css_selector("div[class='zoom__button _type_in']")
+        self.web_driver_wait(5, 'div[class="online__map"]')
+        n = 0
+        while n < value:
+            self.web_driver_wait(5, 'div[class="online__map"]')
+            zoom_button.click()
+            n += 1
 
     def test_search_good_date_input(self):
         """Тест Кейс1:
@@ -27,13 +40,14 @@ class FiveTestsCases(TestCase):
             3. Нажать enter
             Ожидаемый результат: Маршрут найден
         """
-        from_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-2']/..//input[@class='suggest__input']")
-        from_field.send_keys('Площадь Кирова')
-        to_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-3']/..//input[@class='suggest__input']")
-        to_field.send_keys('Версаль')
-        to_field.submit()
-        rout_list = self.driver.find_element_by_xpath("//div[@class='routeResults__wrap']")
-        self.assertTrue('Площадь Кирова' and 'Площадь Кирова' in rout_list.text)
+        obj1 = 'Площадь Кирова'
+        obj2 = 'Версаль'
+        self.web_driver_wait(5, ".searchBar__formsIn")
+        self.search_bar_from_field.send_keys(obj1)
+        self.search_bar_to_field.send_keys(obj2)
+        self.search_bar_to_field.submit()
+        rout_list = self.driver.find_element_by_css_selector(".routeResults")
+        self.assertTrue(obj1 and obj2 in rout_list.text)
 
     def test_search_bad_date_input(self):
         """Тест Кейс2:
@@ -49,43 +63,36 @@ class FiveTestsCases(TestCase):
             Ожидаемый результат:
             Появилась панель: “Увы, невозможно построить такой маршрут”
         """
-        from_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-2']/..//input[@class='suggest__input']")
-        from_field.send_keys('алллаитл')
-        to_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-3']/..//input[@class='suggest__input']")
-        to_field.send_keys('лрдод')
-        to_field.submit()
-        self.assertTrue(self.driver.find_element_by_xpath("//div[@class='noResults__scroller _scrollbar']"))
+        obj1 = 'алллаитл'
+        obj2 = 'лрдод'
+        self.web_driver_wait(5, ".searchBar__formsIn")
+        self.search_bar_from_field.send_keys(obj1)
+        self.search_bar_to_field.send_keys(obj2)
+        self.search_bar_to_field.submit()
+        self.assertTrue(self.driver.find_element_by_css_selector(".noResults__routeType"))
 
     def test_search_map_click_date_input(self):
         """Тест Кейс3:
-            Проверка поиска указанного маршрута
+            Проверка поиска указанного маршрута кликом по карте
             Предусловия:
             1. Доступ в интернет
             2. Перейти на сайте 2gis.ru/novosibirsk в firefox
             3. Выбрать пункт "Проезд"
             Шаги:
-            1. Выберете первое здание на карте откуда строить маршрут
-            2. Нажмите кнопку "Путь отсюда" на появившейся инф. панели выбранного объекта
-            3. Выберете второе здание на карте куда строить маршрут
-            4. Нажмите кнопку "сюда" на появившейся инф. панели выбранного объекта
+            1. Приблизте карту, чтобы на ней появились объекты
+            2. Кликните на первое место/объект откуда строить маршрут
+            3. Кликните на второе место/объект куда строить маршрут
             Ожидаемый результат:
             Появилась панель: Маршрут найден
         """
-        zoom_button = self.driver.find_element_by_xpath("//div[@class='zoom__eventArea zoom__inButton']")
-        n = 0
-        while n < 5:
-            time.sleep(1)
-            zoom_button.click()
-            n += 1
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//img[@src='http://tile0.maps.2gis.com/tiles?x=47857&y=20719&z=16&v=1']")))
-        from_field = self.driver.find_element_by_xpath("//img[@src='http://tile0.maps.2gis.com/tiles?x=47857&y=20719&z=16&v=1']")
-        from_field.click()
-        time.sleep(1)
-        to_field = self.driver.find_element_by_xpath("//img[@src='http://tile2.maps.2gis.com/tiles?x=47858&y=20720&z=16&v=1']")
-        to_field.click()
-        self.assertTrue(self.driver.find_element_by_xpath("//div[@class='map__markerRouteSearch  _type_afoot _city_novosibirsk']/..//div[@class='map__markerRouteSearchPin']").is_displayed())
-        self.assertTrue(self.driver.find_element_by_xpath("//div[@class='map__markerRouteSearch _end  _type_afoot _city_novosibirsk']/..//div[@class='map__markerRouteSearchPin']").is_displayed())
-        self.assertTrue(self.driver.find_element_by_xpath("//div[@class='leaflet-pane leaflet-route-pane']").is_displayed())
+        from_obj = "img[src='http://tile0.maps.2gis.com/tiles?x=47857&y=20719&z=16&v=1']"
+        to_obj = "img[src='http://tile2.maps.2gis.com/tiles?x=47858&y=20720&z=16&v=1']"
+        self.zoom(5)
+        self.web_driver_wait(5, from_obj)
+        self.driver.find_element_by_css_selector(from_obj).click()
+        self.web_driver_wait(5, ".map__markerRouteSearchPin")
+        self.driver.find_element_by_css_selector(to_obj).click()
+        self.assertTrue(self.driver.find_element_by_css_selector("path[class='routeSearchGeometry leaflet-interactive']").is_displayed())
 
     def test_search_from_different_objects(self):
         """Тест Кейс4:
@@ -102,15 +109,14 @@ class FiveTestsCases(TestCase):
 
             Ожидаемый результат: Указанный маршрут построен
         """
-        car_button = self.driver.find_element_by_xpath("//div[@class='searchBar__transportButton searchBar__transportCar']")
-        car_button.click()
-        from_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-2']/..//input[@class='suggest__input']")
-        from_field.send_keys('Виктора Уса 9')
-        to_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-3']/..//input[@class='suggest__input']")
-        to_field.send_keys('Телецентр Остановка')
-        to_field.submit()
-        rout_list = self.driver.find_element_by_xpath("//div[@class='autoResults__wrap']")
-        self.assertTrue('Виктора Уса 9' and 'Телецентр Остановка' in rout_list.text)
+        obj1 = 'Виктора Уса 9'
+        obj2 = 'Телецентр Остановка'
+        self.driver.find_element_by_css_selector(".searchBar__transportCar").click()
+        self.search_bar_from_field.send_keys(obj1)
+        self.search_bar_to_field.send_keys(obj2)
+        self.search_bar_to_field.submit()
+        rout_list = self.driver.find_element_by_css_selector(".autoResults")
+        self.assertTrue(obj1 and obj2 in rout_list.text)
 
     def test_search_subway_option(self):
         """Тест Кейс5:
@@ -127,16 +133,14 @@ class FiveTestsCases(TestCase):
             3.	Нажать кнопку "Найти"
             Ожидаемый результат: Маршрут найден
         """
-        subway_button = self.driver.find_element_by_xpath("//div[@class='searchBar__transportButton searchBar__transportSubway']")
-        subway_button.click()
-        from_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-2']/..//input[@class='suggest__input']")
-        from_field.send_keys('Студенческая')
-        to_field = self.driver.find_element_by_xpath("//div[@id='module-1-1-3']/..//input[@class='suggest__input']")
-        to_field.send_keys('Площадь Маркса')
-        search_button = self.driver.find_element_by_xpath("//button[@class='searchBar__submit _rs']")
-        search_button.click()
-        rout_list = self.driver.find_element_by_xpath("//div[@class='routeResults__wrap']")
-        self.assertTrue('Студенческая' and 'Площадь Маркса' in rout_list.text)
+        obj1 = 'Студенческая'
+        obj2 = 'Маркса'
+        self.driver.find_element_by_css_selector(".searchBar__transportSubway").click()
+        self.search_bar_from_field.send_keys(obj1)
+        self.search_bar_to_field.send_keys(obj2)
+        self.driver.find_element_by_css_selector(".searchBar__submit._rs").click()
+        rout_list = self.driver.find_element_by_css_selector(".routeResults")
+        self.assertTrue(obj1 and obj2 in rout_list.text)
 
     def tearDown(self):
         self.driver.quit()
